fe7a1a124bd51a55e7400d1194e8cec5f1c68d41
Valtemerda <47465328+Valtemerda@users.noreply.github.com>
2019-05-25 20:19:29 +0800

    Add files via upload

---
+++ Machine Problem 2.cpp
@@ -0,0 +1,35 @@
+//Machine Problem 2 Group 4
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+	float a, b, c;
+	float angle1, angle1D, angle2, angle2D, angle3, cosA, cosB;
+	
+	//user inputs the sides
+	cout << "Enter first side: "; cin >> a;
+	cout << "Enter second side: "; cin >> b;
+	cout << "Enter third side: "; cin >> c;
+	
+	//first angle
+	cout << setprecision(2) << fixed;
+	cosA = ((pow(b, 2) + pow(c, 2)) - pow(a, 2)) / (2*(b*c));
+	angle1 = acos(cosA);
+	angle1D = (angle1 * 180) / 3.1415;
+	
+	//second angle
+	cosB = ((pow(c, 2) + pow(a, 2)) - pow(b, 2)) / (2*(c*a));
+	angle2 = acos(cosB);
+	angle2D = (angle2 * 180) / 3.1415;
+	
+	
+	cout << "\nFirst Angle Is: " << setw(20) << "Second Angle Is: " << setw(20) << "Third Angle Is: " << endl;
+	cout << angle1 << " Radians" << setw(10) << angle2 << " Radians" << endl;
+	cout << angle1D << " Degrees" << setw(10) << angle2D << " Degrees";
+	
+	
+	return 0;
+}
